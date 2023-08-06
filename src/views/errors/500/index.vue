55e6724a38e6238eddd2de885e7d407aef983238
seaung <seaung.c0m7de@protonmail.com>
2023-08-06 23:01:26 +0800

    push code

---
+++ src/views/errors/500/index.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+function goHome() {
+    router.push('/')
+}
+</script>
+<template>
+    <div class="flex h-full">
+        <el-button type="primary" @click="goHome">Go To Home</el-button>
+    </div>
+</template>
